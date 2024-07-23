94c0a159ada05ca4bd93c1c04ee8aec0435af2e8
nitishsingh41 <45527813+nitishsingh41@users.noreply.github.com>
2024-07-23 12:19:21 +0530

    Update app_old.py

--- app_old.py
+++ app_old.py
@@ -43,14 +43,30 @@ if st.button("Submit"):
                 st.error("Please enter a valid website URL.")
                 source_path = None
         
-        #if source_path is not None:
+        if source_path is not None:
             # Ask a question
-        if question := st.chat_input("Ask a question"):
-            # Append user question to history
-            st.chat_message("user").markdown(question)
-            st.chat_message("assistant").markdown("ans: ",question)
-            # Add user question
-    
+            if "messages" not in st.session_state:
+                st.session_state.messages = []
+            
+            # Display chat messages from history on app rerun
+            for message in st.session_state.messages:
+                with st.chat_message(message["role"]):
+                    st.markdown(message["content"])
+            
+            # React to user input
+            if prompt := st.chat_input("What is up?"):
+                # Display user message in chat message container
+                st.chat_message("user").markdown(prompt)
+                # Add user message to chat history
+                st.session_state.messages.append({"role": "user", "content": prompt})
+            
+                response = f"Echo: {prompt}"
+                # Display assistant response in chat message container
+                with st.chat_message("assistant"):
+                    st.markdown(response)
+                # Add assistant response to chat history
+                st.session_state.messages.append({"role": "assistant", "content": response})
+
       
 
 
